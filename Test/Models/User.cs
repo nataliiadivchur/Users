21f792eff5aade2ea16e0ff1420544a40076a6b0
Nataliia <nataliadivchur@gmail.com>
2021-11-06 14:04:30 +0200

    Init commit

---
+++ Test/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+        
+    }
+}
